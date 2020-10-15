993b61574ba7f6256438159f7b264f8f4c01a83a
ayat-tareq <ayaatariq203@gmail.com>
2020-10-15 17:06:35 +0300

    local data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import MapGL, { LanguageControl } from '@urbica/react-map-gl';
+import React, { useState, useEffect } from "react";
+import MapGL, { LanguageControl ,Marker ,Popup } from '@urbica/react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { setRTLTextPlugin } from "mapbox-gl";
+import * as parkData from './data/skateboard-parks.json';
 setRTLTextPlugin(
   'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
   null,
@@ -10,30 +11,69 @@ setRTLTextPlugin(
 
 
 
-
-class App extends React.Component {
-  state={
-  style:{ width: '100vw', height: '100vh' },
-  mapStyle:'mapbox://styles/ayat-1997/ckg9douwf06vj19qsp2q658rs/draft',
-  accessToken:'pk.eyJ1IjoiYXlhdC0xOTk3IiwiYSI6ImNrZzhnNnFzOTBoMWYyd3FvcTE5eTRudDMifQ.P3qIkZkNtFIrJN6uTWKCcw',
-  latitude:33.318796666666664,
-  longitude:44.30550899999999,
-  zoom:11}
+function App(){
+  const [viewport, setViewport] = useState({
+  
+  latitude:45.4211,
+  longitude:-75.6983,
+  zoom:11});
+  const [selectedPark, setSelectedPark] = useState(null);
    
-  render(){
-  return (
-    <div>
- 
-  <MapGL {...this.state}
-  onViewportChange={viewport => {
-    this.setState(viewport);}}
+  
     
-  >
-
+   
+  return (
+    
+  <MapGL {...viewport}
+  style={ {width: '100vw', height: '100vh'} }
+  mapStyle='mapbox://styles/ayat-1997/ckg9douwf06vj19qsp2q658rs/draft'
+  accessToken='pk.eyJ1IjoiYXlhdC0xOTk3IiwiYSI6ImNrZzhnNnFzOTBoMWYyd3FvcTE5eTRudDMifQ.P3qIkZkNtFIrJN6uTWKCcw'
+  onViewportChange={viewport => {
+    setViewport(viewport);}}>
     <LanguageControl language='ar' defaultLanguage='ar' />
+    
+  
+    {parkData.features.map((itme)=>{
+      return(
+        <Marker key={itme.properties.PARK_ID}
+         latitude={itme.geometry.coordinates[1]}
+        longitude={itme.geometry.coordinates[0]}
+        >
+        <button
+              className="markr-btn"
+              onClick={e => {
+                e.preventDefault();
+                setSelectedPark(itme);
+              }}
+            >
+              <img src="cell-tower.svg" alt="cell-tower" />
+            </button>
+        </Marker>
+      )
+    } 
+         )
+    
+  }
+
+{selectedPark ? (
+          <Popup
+            latitude={selectedPark.geometry.coordinates[1]}
+            longitude={selectedPark.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedPark(null);
+            }}
+          >
+            <div>
+              <h2>{selectedPark.properties.NAME}</h2>
+              <p>{selectedPark.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        ) : null}
+  
   </MapGL>
-    </div>
+  
   );}
-}
+
+
 
 export default App;
